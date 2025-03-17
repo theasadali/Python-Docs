0d5419c1f1c46bd738b070d3e8d7fcb70a3cc77b
Asad Ali <67062577+theasadali@users.noreply.github.com>
2025-03-17 12:03:41 +0500

    for loop on dictionaries

--- 4.2-for-statements.py
+++ 4.2-for-statements.py
@@ -4,6 +4,17 @@ for w in words:
     print(f'{w}, {len(w)}')
 
 
-for i in range(10):
-    print(i)
-
+users_dict = {
+    'user1': 'active',
+    'user2': 'inactive',
+    'user3': 'inactive',
+    'user4': 'active',
+    'user5': 'inactive'
+}
+print("--printing user dic before")
+print(users_dict)
+for user,status in users_dict.copy().items():
+    if status == 'inactive':
+        del(users_dict[user])
+        print("--printing user dic after")
+        print(users_dict)
